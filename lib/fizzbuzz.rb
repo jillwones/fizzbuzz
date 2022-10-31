1dda8e12839426e067abf1730794b76137a1a75b
Faris Taher <faris_taher@outlook.com>
2022-10-31 15:44:38 +0000

    added tests for 3 and 5

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,4 @@
+def fizzbuzz(number)
+  return "fizz" if (number % 3).zero?
+  return "buzz" if (number % 5).zero?
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe 'fizzbuzz' do
   it "when passed 3 returns fizz" do
     expect(fizzbuzz(3)).to eq("fizz")
   end
+
+  it "when passed 5 returns buzz" do
+    expect(fizzbuzz(5)).to eq("buzz")
+  end
 end
