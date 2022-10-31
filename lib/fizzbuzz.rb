4a5d8ca06557495add8f966265551f3bb8e92cf7
williamjones <willj836@gmail.com>
2022-10-31 16:21:28 +0000

    cleaned up fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,30 +1,32 @@
+# def fizzbuzz(number)
+#   return "fizzbuzz" if (number % 15).zero?
+#   return "fizz" if (number % 3).zero?
+#   return "buzz" if (number % 5).zero?
+#   number
+# end
+
+# def fizzbuzz(number)
+#   if number % 15 == 0
+#     'fizzbuzz'
+#   elsif number % 5 == 0
+#     'buzz'
+#   elsif number % 3 == 0
+#     'fizz'
+#   else 
+#     number 
+#   end 
+# end
+
+# def fizzbuzz(number)
+#   case
+#   when number % 15 == 0 then "fizzbuzz"
+#   when number % 5 == 0  then "buzz"
+#   when number % 3 == 0 then "fizz"
+#   else number end
+# end 
+
 class Integer
-  # def fizzbuzz(number)
-  #   return "fizzbuzz" if (number % 15).zero?
-  #   return "fizz" if (number % 3).zero?
-  #   return "buzz" if (number % 5).zero?
-  #   number
-  # end
-
-  # def fizzbuzz(number)
-  #   if number % 15 == 0
-  #     'fizzbuzz'
-  #   elsif number % 5 == 0
-  #     'buzz'
-  #   elsif number % 3 == 0
-  #     'fizz'
-  #   else 
-  #     number 
-  #   end 
-  # end
-
-  # def fizzbuzz(number)
-  #   case
-  #   when number % 15 == 0 then "fizzbuzz"
-  #   when number % 5 == 0  then "buzz"
-  #   when number % 3 == 0 then "fizz"
-  #   else number end
-  # end 
 
   def fizzbuzz() (self % 15).zero? ? 'fizzbuzz' : (self % 5).zero? ? 'buzz' : (self % 3).zero? ? 'fizz' : self end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,19 +5,35 @@ describe 'fizzbuzz' do
     expect(3.fizzbuzz).to eq("fizz")
   end
 
+  it "when passed 6 returns fizz" do 
+    expect(6.fizzbuzz).to eq("fizz")
+  end
+
   it "when passed 5 returns buzz" do
     expect(5.fizzbuzz).to eq("buzz")
+  end
+
+  it "when passed 10 returns buzz" do 
+    expect(10.fizzbuzz).to eq("buzz")
   end
 
   it "when passed 15 returns fizzbuzz" do 
     expect(15.fizzbuzz).to eq("fizzbuzz")
   end
 
+  it "when passed 45 returns fizzbuzz" do 
+    expect(45.fizzbuzz).to eq("fizzbuzz")
+  end 
+
   it "returns 7 when passed 7" do 
     expect(7.fizzbuzz).to eq(7)
   end
 
-  it "returns fizz when passed 3" do
-    expect(3.fizzbuzz).to eq("fizz")
+  it "returns 11 when passed 11" do 
+    expect(11.fizzbuzz).to eq(11)
+  end
+
+  it "returns fizz when passed 28" do
+    expect(28.fizzbuzz).to eq(28)
   end
 end
