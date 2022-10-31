ae6cf8bbf979c346c98af551f777f3ce3aea6fe2
Faris Taher <faris_taher@outlook.com>
2022-10-31 16:05:57 +0000

    changed fizzbuzz to an Integer method

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,18 +2,22 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it "when passed 3 returns fizz" do
-    expect(fizzbuzz(3)).to eq("fizz")
+    expect(3.fizzbuzz).to eq("fizz")
   end
 
   it "when passed 5 returns buzz" do
-    expect(fizzbuzz(5)).to eq("buzz")
+    expect(5.fizzbuzz).to eq("buzz")
   end
 
   it "when passed 15 returns fizzbuzz" do 
-    expect(fizzbuzz(15)).to eq("fizzbuzz")
+    expect(15.fizzbuzz).to eq("fizzbuzz")
   end
 
   it "returns 7 when passed 7" do 
-    expect(fizzbuzz(7)).to eq(7)
+    expect(7.fizzbuzz).to eq(7)
+  end
+
+  it "returns fizz when passed 3" do
+    expect(3.fizzbuzz).to eq("fizz")
   end
 end
