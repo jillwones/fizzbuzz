732b04949737048012554426d975ac38924848d3
williamjones <willj836@gmail.com>
2022-10-31 15:57:44 +0000

    passing all tests built around spec, refactored down to 1 line

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,28 @@
-def fizzbuzz(number)
-  return "fizz" if (number % 3).zero?
-  return "buzz" if (number % 5).zero?
-end
+# def fizzbuzz(number)
+#   return "fizzbuzz" if (number % 15).zero?
+#   return "fizz" if (number % 3).zero?
+#   return "buzz" if (number % 5).zero?
+#   number
+# end
+
+# def fizzbuzz(number)
+#   if number % 15 == 0
+#     'fizzbuzz'
+#   elsif number % 5 == 0
+#     'buzz'
+#   elsif number % 3 == 0
+#     'fizz'
+#   else 
+#     number 
+#   end 
+# end
+
+# def fizzbuzz(number)
+#   case
+#   when number % 15 == 0 then "fizzbuzz"
+#   when number % 5 == 0  then "buzz"
+#   when number % 3 == 0 then "fizz"
+#   else number end
+# end 
+
+def fizzbuzz(number) (number % 15).zero? ? 'fizzbuzz' : (number % 5).zero? ? 'buzz' : (number % 3).zero? ? 'fizz' : number end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,12 @@ describe 'fizzbuzz' do
   it "when passed 5 returns buzz" do
     expect(fizzbuzz(5)).to eq("buzz")
   end
+
+  it "when passed 15 returns fizzbuzz" do 
+    expect(fizzbuzz(15)).to eq("fizzbuzz")
+  end
+
+  it "returns 7 when passed 7" do 
+    expect(fizzbuzz(7)).to eq(7)
+  end
 end
